d6cd2de5c2501cefe1709d3c447480ebb72819d3
Dustin Hennessy <Hennessydustin@gmail.com>
2015-07-31 11:50:59 -0400

    Initial Commit

---
+++ SLUGGR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SLUGGR
+//
+//  Created by Dustin Hennessy on 7/31/15.
+//  Copyright (c) 2015 DustinHennessy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
