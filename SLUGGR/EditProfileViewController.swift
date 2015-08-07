791e16835028eeedc700f75e11a7a66ce47fd701
Dustin Hennessy <Hennessydustin@gmail.com>
2015-08-07 13:48:32 -0400

    Made login fields more dynamic and powerful.

--- SLUGGR/EditProfileViewController.swift
+++ SLUGGR/EditProfileViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class EditProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    var profileTableView :UITableView!
-    var dynamicLabelArray = ["First Name", "Last Name", "Home Address", "Work Address", "Driver Status", "Morning Departure Time", "Evening Departure Time", "Bio", "Preferences", "Your Map"]
+    @IBOutlet var profileTableView :UITableView!
+    var dynamicLabelArray = ["First Name", "Last Name", "Home Address", "Work Address", "Morning Depart Time", "Evening Depart Time", "Driver Status","Bio", "Preferences", "Your Map"]
     var dataFieldTypeArray = ["TextFieldCell", "TextFieldCell", "TextFieldCell", "TextFieldCell", "TextFieldCell", "TextFieldCell", "SwitchCell","TextViewCell", "TextViewCell", "MapCell"]
+    var tapped = false
     
     //#2 geocode the home and work locations so we can annotate on the profileMapView
     
@@ -22,6 +23,43 @@ class EditProfileViewController: UIViewController, UITableViewDataSource, UITabl
         return dynamicLabelArray.count
     }
     
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let currentCellType = dataFieldTypeArray[indexPath.row];
+        switch currentCellType {
+        case "TextViewCell":
+            return 120
+        case "MapCell":
+            return 180
+        case "DateCell":
+            return 98
+        default:
+            return 45
+        }
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        var cell = profileTableView.cellForRowAtIndexPath(indexPath)
+        if cell is ProfileTextFieldTableViewCell {
+            let tfCell = cell as! ProfileTextFieldTableViewCell
+            if tfCell.dynamicTFCLabel.text == "Morning Depart Time" || tfCell.dynamicTFCLabel.text == "Evening Depart Time" {
+                let nextIndexPath = NSIndexPath(forRow: indexPath.row + 1, inSection: indexPath.section)
+                var nextCell = profileTableView.cellForRowAtIndexPath(nextIndexPath)
+                if nextCell is ProfileDateTableViewCell {
+                    dynamicLabelArray.removeAtIndex(indexPath.row + 1)
+                    dataFieldTypeArray.removeAtIndex(indexPath.row + 1)
+                    let tdCell = nextCell as! ProfileDateTableViewCell
+                    tdCell.cellDatePicker.hidden = true
+                    profileTableView.deleteRowsAtIndexPaths([nextIndexPath], withRowAnimation: UITableViewRowAnimation.Top)
+                } else {
+                    dynamicLabelArray.insert("Start Time", atIndex: indexPath.row + 1)
+                    dataFieldTypeArray.insert("DateCell", atIndex: indexPath.row + 1)
+                    profileTableView.insertRowsAtIndexPaths([nextIndexPath], withRowAnimation: UITableViewRowAnimation.Bottom)
+                }
+                // FIX THIS
+                profileTableView.reloadRowsAtIndexPaths([nextIndexPath], withRowAnimation: UITableViewRowAnimation.Bottom)
+            }
+        }
+    }
     
     func tableFieldChanged(sender: AnyObject) {
         let dataFieldPosition = sender.convertPoint(CGPointZero, toView: profileTableView)
@@ -48,54 +86,62 @@ class EditProfileViewController: UIViewController, UITableViewDataSource, UITabl
         
     }
     
-    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        switch indexPath.row {
-        case 0,1,2,3,4,5:
-            var textFieldCell = tableView.dequeueReusableCellWithIdentifier("TextFieldCell") as! ProfileTextFieldTableViewCell
+        let currentCellType = dataFieldTypeArray[indexPath.row];
+        switch currentCellType {
+        case "TextFieldCell":
+            var textFieldCell = tableView.dequeueReusableCellWithIdentifier(currentCellType) as! ProfileTextFieldTableViewCell
             textFieldCell.dynamicTFCLabel.text = dynamicLabelArray[indexPath.row]
-            textFieldCell.dynamicProfileTextField.addTarget(self, action: "tableFieldChanged", forControlEvents: UIControlEvents.EditingChanged)
+            textFieldCell.dynamicProfileTextField.addTarget(self, action: "tableFieldChanged:", forControlEvents: UIControlEvents.EditingChanged)
+            textFieldCell.selectionStyle = UITableViewCellSelectionStyle.None
             return textFieldCell
-        case 6:
-            var switchCell = tableView.dequeueReusableCellWithIdentifier("SwitchCell") as! ProfileSwitchTableViewCell
+        case "SwitchCell":
+            var switchCell = tableView.dequeueReusableCellWithIdentifier(currentCellType) as! ProfileSwitchTableViewCell
             switchCell.dynamicSCLabel.text = dynamicLabelArray[indexPath.row]
-            switchCell.cellSwitch.addTarget(self, action: "tableFieldChanged", forControlEvents: UIControlEvents.ValueChanged)
+            switchCell.cellSwitch.addTarget(self, action: "tableFieldChanged:", forControlEvents: UIControlEvents.ValueChanged)
+            switchCell.selectionStyle = UITableViewCellSelectionStyle.None
             return switchCell
-        case 7,8:
-            var textViewCell = tableView.dequeueReusableCellWithIdentifier("TextViewCell") as! ProfileTextViewTableViewCell
+        case "TextViewCell":
+            var textViewCell = tableView.dequeueReusableCellWithIdentifier(currentCellType) as! ProfileTextViewTableViewCell
             textViewCell.dynamicTVCLabel.text = dynamicLabelArray[indexPath.row]
             //textViewCell.dynamicTextView. the TEXT VIEW ISNT READY YET!
+            textViewCell.selectionStyle = UITableViewCellSelectionStyle.None
             return textViewCell
-        case 9:
-            var mapViewCell = tableView.dequeueReusableCellWithIdentifier("MapCell") as! ProfileMapTableViewCell
+        case "MapCell":
+            var mapViewCell = tableView.dequeueReusableCellWithIdentifier(currentCellType) as! ProfileMapTableViewCell
             //mapViewCell.profileMapView.addAnnotations(<#annotations: [AnyObject]!#>) add home and work annotations
+            mapViewCell.selectionStyle = UITableViewCellSelectionStyle.None
+
             return mapViewCell
+        case "DateCell":
+            var dateViewCell = tableView.dequeueReusableCellWithIdentifier(currentCellType) as! ProfileDateTableViewCell
+            dateViewCell.cellDatePicker.hidden = false
+            // SET DATE HERE
+            dateViewCell.selectionStyle = UITableViewCellSelectionStyle.None
+
+            return dateViewCell
         default:
-            var cell = tableView.dequeueReusableCellWithIdentifier("Cell") as!
-            UITableViewCell
+            var cell = tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell
+            cell.selectionStyle = UITableViewCellSelectionStyle.None
             return cell
         }
     }
     
+    
+    //MARK: - Data to API
+    
+    func saveDataToAPI(sender: UIBarButtonItem) {
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//         profileTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        profileTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
